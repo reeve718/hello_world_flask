037f43e5f8c69e5b0b01eea687c5e7b9e262fdf3
reeve718 <42854263+reeve718@users.noreply.github.com>
2022-01-10 01:46:15 -0700

    petshop

--- src/wisdompets/urls.py
+++ src/wisdompets/urls.py
@@ -1,7 +1,7 @@
-"""hello URL Configuration
+"""wisdompets URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include
 from django.urls import path
-
+from adoptions import views
 
 urlpatterns = [
-    path("", include("pages.urls")),
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('adoptions/<int:pet_id>/', views.pet_detail, name='pet_detail')
 ]
